9343b258b27261d58eb9dcf818a2796a7b95b52a
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-12-06 19:36:11 +0100

    Add files via upload

---
+++ app/app/KlasaC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ *Definirajte klasu KlasaC sa sljedećim članicama:
+• metoda
+int Zbroj(int []) koja će vratiti zbroj svih članova proslijeñenog niza.
+double Prosjek(int []) koja će vratiti prosječnu vrijednost svih članova
+proslijeñenog niza.
+Definirajte objekt tipa KlasaC, zatražite od korisnika da unese 5 cijelih brojeva koje spremite u
+niz tipa int veličine 5 članova. Taj niz proslijedite metodama Zbroj i Prosjek koje ćete pozvati
+za definirani objekt, te povratne vrijednosti spremite u lokalne varijable zbroj i prosjek.
+*/
+namespace app
+{
+    class KlasaC
+    {
+        public int Zbroj(int[] arr)
+        {int zbr = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                zbr += arr[i];
+            }
+         
+            return zbr; 
+        }
+        public double Prosjek(int[] prosjek) 
+        {int sum = 0;
+            for (int i = 0; i < prosjek.Length; i++)
+            {
+                sum += prosjek[i];
+            }
+         
+            return sum / prosjek.Length;
+        }
+
+    }
+}
---
+++ app/app/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+/*
+Definirajte klasu KlasaC sa sljedećim članicama:
+• metoda
+int Zbroj(int []) koja će vratiti zbroj svih članova proslijeñenog niza.
+double Prosjek(int []) koja će vratiti prosječnu vrijednost svih članova
+proslijeñenog niza.
+Definirajte objekt tipa KlasaC, zatražite od korisnika da unese 5 cijelih brojeva koje spremite u
+niz tipa int veličine 5 članova. Taj niz proslijedite metodama Zbroj i Prosjek koje ćete pozvati
+za definirani objekt, te povratne vrijednosti spremite u lokalne varijable zbroj i prosjek.
+*/
+        static void Main(string[] args)
+        {
+            int[] zbrojPros = new int[5]; 
+            
+            Console.WriteLine("Upisite 5 cijelih brojeva:"); 
+            
+            for (int i = 0; i < 5; i++) 
+            { 
+                string s = Console.ReadLine();
+                zbrojPros[i] = Convert.ToInt32(s); 
+            }
+            
+            KlasaC x = new KlasaC(); 
+           
+            int zbroj = x.Zbroj(zbrojPros);
+            double prosjek = x.Prosjek(zbrojPros);
+            
+            Console.WriteLine("Zbroj "+zbroj+", prosjek "+prosjek);
+           
+            
+            
+            
+        }
+    }
+}
